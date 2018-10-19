671c7733c285f8163cf4694f23b8ed75b5109fbe
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-10-19 20:33:36 +0200

    Comprueba y conserva el valor del campo

--- index.php
+++ index.php
@@ -5,27 +5,25 @@
     <title>Tabla de multiplicar</title>
   </head>
   <body>
+    <?php $numero = isset($_GET['num']) ? $numero = trim($_GET['num']) : $numero = ""; ?>
     <form action="" method="get">
-      <label for:="num">Número:</label>
-      <input id ="num" type="text" name="num"> <!--id y name no tienen por qué llamarse igual, pero for e id sí-->
+      <label for="num">Número:</label>
+      <input id ="num" type="text" name="num" value="<?= $_GET['num'] ?>"> <!--id y name no tienen por qué llamarse igual, pero for e id sí-->
       <input type="submit" value="Calcular"> <!--Los botones a menos que sea interesante los vamos a hacer sin name-->
     </form>
     <?php
     require './auxiliar.php';
 
-    if(!isset($_GET['num'])) {
-      mostrarError("falta el parámetro <i>num</i>.");
-    } else {
-      $numero = $_GET ['num'];
-      if (!ctype_digit($numero)) {
-        mostrarError("se ha pasado algo que no es un número.");
-      } else {
-        if ($numero < 0 || $numero > 10) {
-          mostrarError("el número debe estar comprendido entre 0 y 10.");
+    if (!empty($numero)) {
+        if (!ctype_digit($numero)) {
+            mostrarError("se ha pasado algo que no es un número.");
         } else {
-          mostrarTabla($numero);
+            if ($numero < 0 || $numero > 10) {
+                mostrarError("el número debe estar comprendido entre 0 y 10.");
+            } else {
+                mostrarTabla($numero);
+            }
         }
-      }
     }
     ?>
   </body>
